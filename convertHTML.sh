dbb6dd00a925fa54cbda0e4509ff13752568c818
tky1026 <imzeus11dev@gmail.com>
2024-09-19 19:18:31 +0800

    first commit

---
+++ convertHTML.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <notebook-file>"
+  exit 1
+fi
+
+NOTEBOOK_FILE="$1"
+OUTPUT_DIR="./public"
+OUTPUT_NAME="index"
+
+# Run nbconvert to convert the notebook to HTML
+jupyter nbconvert --output-dir="$OUTPUT_DIR" --output="$OUTPUT_NAME" --to html "$NOTEBOOK_FILE"
